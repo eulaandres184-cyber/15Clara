8800030e6381285c7cb5971126c64cb999ef21f5
eulaandres184-cyber <eulaandres184@gmail.com>
2026-03-28 00:31:46 -0300

    cambios firebase1

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -3,21 +3,28 @@
  * Realtime Database para almacenar y sincronizar canciones
  */
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAPeWzo44LOpkZTDKMnzKYNCQrnEZCO71s",
-    authDomain: "claramisxv-103c6.firebaseapp.com",
-    databaseURL: "https://claramisxv-103c6-default-rtdb.firebaseio.com",
-    projectId: "claramisxv-103c6",
-    storageBucket: "claramisxv-103c6.firebasestorage.app",
-    messagingSenderId: "788183965642",
-    appId: "1:788183965642:web:4e0f09c484673469b3b933",
-    measurementId: "G-XWRBPKZW4Q"
+  apiKey: "AIzaSyAPeWzo44LOpkZTDKMnzKYNCQrnEZCO71s",
+  authDomain: "claramisxv-103c6.firebaseapp.com",
+  databaseURL: "https://claramisxv-103c6-default-rtdb.firebaseio.com",
+  projectId: "claramisxv-103c6",
+  storageBucket: "claramisxv-103c6.firebasestorage.app",
+  messagingSenderId: "788183965642",
+  appId: "1:788183965642:web:4e0f09c484673469b3b933",
+  measurementId: "G-XWRBPKZW4Q"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Referencia a Realtime Database
 const database = firebase.database();
